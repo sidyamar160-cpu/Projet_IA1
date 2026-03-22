1e814cd29849ec04f7688133f891f9897c7c9f9a
Sidy <sidyamar160@gmail.com>
2026-03-21 22:06:38 -0400

    Apercu de nos données et Statis_descriptives

--- beans_pods.py
+++ beans_pods.py
@@ -25,3 +25,29 @@ region = st.sidebar.selectbox(
     "Choisissez la région",
     ["Toutes"] + list(data["Region"].unique())
 )
+
+data_filtre = data.copy()
+
+if chaines != "Tous":
+    data_filtre = data_filtre[data_filtre["Channel"] == chaines]
+
+if region != "Toutes":
+    data_filtre = data_filtre[data_filtre["Region"] == region]
+
+
+st.markdown("""
+<div style='text-align: center;'>
+<h1>Beans & Pods </h1>
+<p style='color:red;'>Analyse interactive des ventes</p>
+</div>
+""", unsafe_allow_html=True)
+
+
+st.subheader("Aperçu des données")
+st.dataframe(data_filtre)
+
+st.subheader("Statistiques descriptives")
+st.write(data_filtre.describe())
+
+
+produits = ["Robusta", "Arabica", "Espresso", "Lungo", "Latte", "Cappuccino"]
